a62f61e25bd4ca453eea5bc2e770825aa9ea5396
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-01-11 05:16:53 -0500

    testo

---
+++ src/calculatricejenkins/CalculatriceJenkins.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculatricejenkins;
+
+/**
+ *
+ * @author ald
+ * 
+ */
+
+public class CalculatriceJenkins {
+    
+    private float initNumber = 0;
+
+    public float getInitNumber() {
+        return initNumber;
+    }
+
+    public void setInitNumber(float initNumber) {
+        this.initNumber = initNumber;
+    }
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public int add(int num, int num1){
+        return num + num1;
+    }
+    
+    public int substract(int num, int num1){
+         return num - num1;
+        
+    }
+    
+    public int divide(int num, int num1){
+         return num / num1;
+        
+    }
+    
+    public int multiply(int num, int num1){
+         return num * num1;
+        
+    }
+    
+    
+}
---
+++ test/calculatricejenkins/CalculatriceJenkinsIT.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package calculatricejenkins;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *class
+ * @author ald
+ */
+public class CalculatriceJenkinsIT {
+    
+    private static CalculatriceJenkins calculatrice;
+    
+    public CalculatriceJenkinsIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        calculatrice = new CalculatriceJenkins();
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class CalculatriceJenkins.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        CalculatriceJenkins.main(args);
+    }
+    
+    @Test
+    public void testMultiply() {
+        assertEquals( "Regular multiplication should work", calculatrice.multiply(4,5), 20);
+    }
+    
+    @Test
+    public void testSubstract() {
+        assertEquals( "Regular substraction should work", calculatrice.substract(10,5), 5);
+    }
+    
+    @Test
+    public void testAdd() {
+        assertEquals( "Regular addition should work", calculatrice.add(4,5), 9);
+    }
+    
+    @Test
+    public void testDivide() {
+        assertEquals( "Regular division should work", calculatrice.divide(20,5), 4);
+    }
+    
+    @Test
+    public void testMultiplyWithZero() {
+        assertEquals("Multiple with zero should be zero",0,  calculatrice.multiply(0,5));
+        assertEquals("Multiple with zero should be zero", 0, calculatrice.multiply(5,0));
+    }
+    
+}
